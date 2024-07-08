32e1ef7468ef5feba49529df82ff537a5031742e
Narcisa-1156 <paolapinzon2514@gmail.com>
2024-07-08 08:22:29 -0500

    Update 01_esquinas.c

--- 01_esquinas.c
+++ 01_esquinas.c
@@ -1,33 +1,39 @@
-#include<stdio.h>
+#include <stdio.h>
+#define M 12
+
+void presentar_esquinas(int matriz[M][M], int filas, int columnas){
+    if (filas > 0 && columnas > 0){
+        printf ("Esquina superior izquierda: %d\n", matriz[0][0]);
+        printf ("Esquina superior derecha: %d\n", matriz[0][columnas -1]);
+        printf ("Esquina inferior izquierda: %d\n", matriz[filas - 1][0]);
+        printf ("Esquina inferior derecha: %d\n", matriz[filas - 1][columnas - 1]);
+    }
+}
 int main(){
-    int n;
-    printf("Programa para identificar las esquinas en una matriz\n");
-    printf("Ingrese la dimensión de la matriz\n");
-    scanf("%d", &n);
-    int  matriz[n][n];
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            printf("Digite el valor en la posición [%d][%d]: ", i, j);
+    int matriz[M][M];
+    int filas, columnas;
+    
+    printf ("Ingrese el numero de filas: ");
+    scanf ("%d", &filas);
+    printf ("Ingrese el numero de columnas: ");
+    scanf ("%d", &columnas);
+
+    for (int i = 0; i < filas; i++){
+        for (int j = 0; j < columnas; j++){
+            printf("Ingrese los valores en la posicion [%d][%d]: ",i, j);
             scanf("%d", &matriz[i][j]);
-        }   
-    }
-    printf("Calculo realizado\n");
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (j == 0 || j == 2)
-            {
-                printf("%d ", matriz[i][j]);
-            }else
-            {
-                printf("-- ");
-            }
-                                
         }
-        printf("\n");       
     }
+    printf("La matriz que ingresada es:\n ");
+
+    for (int i = 0; i < filas; i++){
+        for (int j = 0; j < columnas; j++){
+            printf("%d\t", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf ("Los elementos de las esquinas son:\n");
+    presentar_esquinas(matriz, filas, columnas);
     return 0;
 }
